8cbde58e01d676245d4a6cf76b49a508b2e1c810
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-10-27 13:55:24 +0100

    seventh test passed

--- src/customer.py
+++ src/customer.py
@@ -1,11 +1,16 @@
+from src.drink import Drink
+
 class Customer:
     def __init__(self, name, wallet):
         self.name = name
-        self.wallet = wallet
+        self.wallet = 100
         self.drinks = 0
     
     def add_drink(self, drink):
         self.drinks += 1
         return self.drinks
+
+    def reduce_wallet(self, amount):
+        self.wallet -= amount
 
     # def buy_drink(self, beverage):
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -7,7 +7,7 @@ class TestCustomer(unittest.TestCase):
     def setUp(self):
         self.customer = Customer("Sam", 100)
         self.drinks = Drink("wine", 25)
-
+        self.wallet = Customer("Sam", 100)
 
     def test_customer_has_name(self):
         self.assertEqual("Sam", self.customer.name)
@@ -18,6 +18,10 @@ class TestCustomer(unittest.TestCase):
     def test_can_add_beverage(self):
         self.customer.add_drink(self.drinks)
         self.assertEqual(1, self.customer.drinks)
+
+    def test_can_reduce_wallet(self):
+        self.customer.reduce_wallet(25)
+        self.assertEqual(75, self.customer.wallet)
 
     # def test_customer_can_buy_beverage(self):
     #     self.customer.
