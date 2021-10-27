46dd05c8b3d1254485bbd1c63dbf2e1ce1e61fbf
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-10-27 11:39:00 +0100

    first test passed

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -4,3 +4,6 @@ from src.pub import Pub
 class TestPub(unittest.TestCase):
     def setUp(self):
         self.pub = Pub("Lucky Pub", 1000, "wine")
+
+    def test_pub_has_name(self):
+        self.assertEqual("Lucky Pub", self.pub.name)
