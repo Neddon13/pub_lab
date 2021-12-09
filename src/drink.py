e517f3fbdac111e9369095d4fc23a5454155ceaa
Almas <m_a_b19@hotmail.com>
2021-12-09 11:27:52 +0000

    Update drink.py

--- src/drink.py
+++ src/drink.py
@@ -1,4 +1,5 @@
 class Drink:
-    def __init__(self, beverage, price):
+    def __init__(self, beverage, price, WAYYYYYYYYY):
         self.beverage = beverage
         self.price = price
+        self.WAYYYYYYYYY = WAYYYYYYY!!!!!!!!!
