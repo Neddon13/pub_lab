78fa1a05742d0178adfa940781709a6cc88454f5
Almas <m_a_b19@hotmail.com>
2021-12-09 11:00:59 +0000

    added new attribute to the drink class

--- src/drink.py
+++ src/drink.py
@@ -1,5 +1,6 @@
 class Drink:
-    def __init__(self, beverage, price, alcohol_level):
+    def __init__(self, beverage, price, alcohol_level, quantity_in_ml):
         self.beverage = beverage
         self.price = price
         self.alcohol_level = alcohol_level
+        self.quantity_in_ml = quantity_in_ml;
