190396b0fb58b6b2048e90fc38601a4fd9c9e783
Almas <m_a_b19@hotmail.com>
2021-12-09 11:20:34 +0000

    drink class 2 attributes removed

--- src/drink.py
+++ src/drink.py
@@ -1,6 +1,4 @@
 class Drink:
-    def __init__(self, beverage, price, alcohol_level, quantity_in_ml):
+    def __init__(self, beverage, price):
         self.beverage = beverage
         self.price = price
-        self.alcohol_level = alcohol_level
-        self.quantity_in_ml = quantity_in_ml;
